62d361687122845df72096910f09d983d438ce63
s32880 <s32880@pjwstk.edu.pl>
2026-04-14 16:03:44 +0200

    Creation of controllers

---
+++ APBD6/APBD6/Controllers/ReservationsController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APBD6.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationsController : ControllerBase
+    {
+    }
+}
