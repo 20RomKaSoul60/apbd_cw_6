e082e426d48b4a4e93906d842cea9082fa76fbf2
s32880 <s32880@pjwstk.edu.pl>
2026-04-14 23:35:48 +0200

    Implementation of RoomsController.cs

--- APBD6/APBD6/Controllers/RoomsController.cs
+++ APBD6/APBD6/Controllers/RoomsController.cs
@@ -1,3 +1,5 @@
+using APBD6.Models;
+using APBD6.Properties;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,5 +9,89 @@ namespace APBD6.Controllers
     [ApiController]
     public class RoomsController : ControllerBase
     {
+        
+        //GETs
+        
+        [HttpGet]
+        public ActionResult<Dictionary<int,Room>> getAllRooms()
+        {
+            return Ok(DB.Rooms);
+
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Room> GetRoom(int id)
+        {
+            var room = DB.Rooms.FirstOrDefault(r=> r.Value.Id == id);
+            if (room.Value == null)
+            {
+                return NotFound("Room was not found"); 
+            }
+            return Ok(room);
+        }
+        
+        [HttpGet("buildings/{buildingCode}")]
+        public ActionResult<Room> getByBuildingCode(int buildingCode)
+        {
+            var room = DB.Rooms.Where(r =>
+                r.Value.BuildingCode == buildingCode &&
+                r.Value.Capacity >= 20 && r.Value.IsActive == true &&
+                r.Value.HasProjector == true).First();
+
+            if (room.Value == null)
+            {
+                return NotFound("Room was not found");
+            }
+            return Ok(room);
+
+        }
+
+
+        // POST
+        [HttpPost]
+        public ActionResult<Room> Create(Room room)
+        {
+            if (String.IsNullOrEmpty(room.Name) || 
+                String.IsNullOrEmpty(room.BuildingCode.ToString()) || 
+                String.IsNullOrEmpty(room.Capacity.ToString()) || 
+                String.IsNullOrEmpty(room.Floor.ToString()) || 
+                String.IsNullOrEmpty(room.Id.ToString()) )
+            {
+                return BadRequest($"Attribute is required to be filled");
+            }
+            if (!DB.Rooms.ContainsValue(room))
+            {
+                return BadRequest("Such room already exists");
+            }
+            DB.Rooms.Add(DB.Rooms.Last().Value.Id+1,room);
+            
+            return Ok("Room was added");
+        }
+        
+        //PUT
+        [HttpPut("{id}")]
+        public ActionResult<Room> Update(int id, Room room)
+        {
+            if (!DB.Rooms.Any(r => r.Value.Id == id))
+            {
+                return NotFound("Room was not found");
+            }
+            DB.Rooms[id] =  room;
+            return Ok("Room was updated");
+            
+        }
+        
+        //DELETE
+        [HttpDelete("{id}")]
+        public ActionResult<Room> Delete(int id)
+        {
+            if (!DB.Rooms.Any(r => r.Value.Id == id))
+            {
+                return NotFound("Room was not found");
+            }
+
+            DB.Rooms.Remove(DB.Rooms.First(r => r.Value.Id == id).Key);
+            return Ok("Room was deleted");
+        }
     }
 }
